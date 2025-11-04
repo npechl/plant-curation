653235580a8b3e6a271760a9988ed645708a93a4
Sofoklis Keisaris <sofoskeisaris@gmail.com>
2025-11-04 11:52:42 +0200

    add optparse

--- BOLD Systems/R/apply_sativa.R
+++ BOLD Systems/R/apply_sativa.R
@@ -1,18 +1,44 @@
 library(data.table)
 
-tax_file <- "test.tax"
-mis_file <- "syntest.mis"
-out_file <- "test_corrected.tax"
+correct_taxonomy <- function(tax_file, mis_file, out_file){
+  tax <- fread(tax_file, header = F, sep = "\t")
+  mis <- fread(mis_file, header = T, sep = "\t")
+  
+  setnames(tax, c("ID", "Taxonomy"))
+  setnames(mis, old = ";SeqID", new = "ID")
+  
+  oldtax <- copy(tax)
+  
+  tax[mis, on = "ID", Taxonomy := ProposedTaxonomyPath]
+  
+  changed <- oldtax[Taxonomy != tax$Taxonomy]
+  
+  fwrite(tax, out_file, sep = "\t", quote = FALSE)
+}
 
-tax <- fread(tax_file, header = F, sep = "\t")
-mis <- fread(mis_file, header = T, sep = "\t")
+# optparse ---------------------------------------------------------------------
 
-setnames(tax, c("ID", "Taxonomy"))
-setnames(mis, old = ";SeqID", new = "ID")
+parser <- OptionParser() |>
+  add_option(c("-t", "--tax"), type = "character",
+              help = "Input taxonomy file (.tax) [required]")|>
+  add_option(c("-m", "--mis"), type = "character",
+              help = "Input misidentification file (.mis) [required]")|>
+  add_option(c("-o", "--output"), type = "character", default = NULL,
+              help = "Output corrected taxonomy file [default: <input>_corrected.tax]")
 
-oldtax <- copy(tax)
-tax[mis, on = "ID", Taxonomy := ProposedTaxonomyPath]
+arguments <- parse_args(parser)
 
-oldtax[Taxonomy != tax$Taxonomy]
+# validate ---------------------------------------------------------------------
+if (is.null(opt$tax) || is.null(opt$mis)) {
+  print_help(opt_parser)
+  stop("\nError: You must specify both --tax and --mis files.\n", call. = FALSE)
+}
 
-fwrite(tax, out_file, sep = "\t", quote = FALSE)
+# default output name ---------------------------
+if (is.null(opt$output)) {
+  base <- sub("\\.tax$", "", basename(opt$tax))
+  opt$output <- paste0(base, "_corrected.tax")
+}
+
+# run -----------------------------------------
+correct_taxonomy(opt$tax, opt$mis, opt$output)
